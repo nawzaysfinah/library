f0bb213c0d83eb831ae8a445789c9abb8b2d12a8
nawzaysfinah <nawzaysfinah@gmail.com>
2023-04-27 16:11:23 +0800

    jaybees tech talk follow through

--- script.js
+++ script.js
@@ -1,25 +1,58 @@
+//  declare empty array
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+// object constructor
+function Book(Title, Author, Pages, Read) {
+  this.Title = Title;
+  this.Author = Author;
+  this.Pages = Pages;
+  this.Read = Read;
+  this.info = Title + ", " + Author + ", " + Pages + ", " + Read;
 }
 
-const book1 = new Book(
-  "The One Straw Revolution",
-  "Masanobu Fukuoka",
-  "200",
-  true
+// function for adding a new book to the array/library
+function addBookToLibrary(Title, Author, Pages, Read) {
+  let book = new Book(Title, Author, Pages, Read);
+  myLibrary.push(book);
+}
+
+// const theHobbit = new Book("The Hobbit", "J.R.R Tolkein", "372", false);
+// console.log(theHobbit.info);
+
+// function to display library array to cards
+function displayBooksOnPage() {
+  const books = document.querySelector(".books");
+}
+
+// Loop over the library array and display to the cards
+myLibrary.forEach((myLibrary) => {
+  const card = document.createElement("div)");
+  card.classList.add("card");
+  books.appendChild(card);
+  for (let key in myLibrary) {
+    console.log(`${key}: ${myLibrary[key]}`);
+    const para = document.createElement("p");
+    para.textContent = `${key}: ${myLibrary[key]}`;
+    card.appendChild(para);
+  }
+});
+
+// Calling function and adding data manually for nwow until the form is created
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read Yet");
+addBookToLibrary(
+  "The Seven Habits of Highly Effective People",
+  "Steven Covey",
+  "200 Pages",
+  "Read"
+);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295 Pages", "Not Read Yet");
+addBookToLibrary(
+  "The Seven Habits of Highly Effective People",
+  "Steven Covey",
+  "200 Pages",
+  "Read"
 );
 
-const book2 = new Book("A Whole New Mind", "Daniel H. Pink", "304", true);
+console.log("End of code array contents", myLibrary);
 
-function addBookToLibrary() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+displayBooksOnPage;
